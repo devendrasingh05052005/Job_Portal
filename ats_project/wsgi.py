f51d1997a02306841869bbbd3ed8aeb1276f5a58
devendra <banad8640@gmail.com>
2025-09-05 16:26:01 +0530

    Added Render deployment files

--- ats_project/wsgi.py
+++ ats_project/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for ats_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
+# ats_project/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ats_project.settings")
+# Whitenoise is now configured in settings.py MIDDLEWARE
+# Isliye ab is import ki zaroorat nahi hai.
+# from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ats_project.settings')
 
 application = get_wsgi_application()
+
+# Whitenoise is configured in middleware, so this line is not needed
+# application = DjangoWhiteNoise(application)
